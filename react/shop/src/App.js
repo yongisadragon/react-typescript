b6c63a8851d68ab3925ca69d3447d3322517cd3f
yongisagragon <ckfflekfxms@gmail.com>
2023-09-06 17:36:35 +0900

    0906 react route 셋팅

--- react/shop/src/App.js
+++ react/shop/src/App.js
@@ -3,6 +3,8 @@ import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import 이미지1 from "./img/18ac3091-5615-4b51-b833-f3d8f909b35c.webp";
 import { useState } from "react";
 import { data } from "./data";
+import { Link, Route, Routes } from "react-router-dom";
+import Detail from "./component/Detail";
 function App() {
   let [shoes] = useState(data);
   return (
@@ -16,16 +18,30 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
-
-      <div
-        className="main-bg"
-        style={{ backgroundImage: `url(${이미지1})` }}
-      ></div>
-      <div className="container">
-        <div className="row">
-          <Item shoes={shoes} />
-        </div>
-      </div>
+      <Link to="/">Home</Link>
+      <Link to="/detail">Detail</Link>
+      <Link to="/about">About</Link>
+      {/* <Routes>는 단일 <Router />를 감싸며 각각의 Router들은 페이지 하나에 해당한다. */}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div
+                className="main-bg"
+                style={{ backgroundImage: `url(${이미지1})` }}
+              ></div>
+              <div className="container">
+                <div className="row">
+                  <Item shoes={shoes} />
+                </div>
+              </div>
+            </>
+          }
+        />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/about" element={<div>어바웃페이지임</div>} />
+      </Routes>
     </div>
   );
 }
