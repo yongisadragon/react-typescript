76d52e9ec083870dff56de3db3c96d2fee931269
Lee Sang Yong <ckfflekfxms@gmail.com>
2023-10-16 16:22:59 +0900

    1016 createSlice, useSelector

--- react/shop/src/pages/Cart.js
+++ react/shop/src/pages/Cart.js
@@ -1,6 +1,18 @@
 import { Table } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 export default function Cart() {
+  // redux store 가져와줌.
+  // let a = useSelector((state) => {
+  //   return state;
+  // });
+  // 이렇게 뽑아서 따로 쓰는게 더 편하겠네요. 중괄호랑 return은 세트로 생략가능
+  const user = useSelector((state) => state.user);
+  const stock = useSelector((state) => state.stock);
+  const products = useSelector((state) => state.products);
+
+  console.log(products);
+
   // tr-행생성, th,td-열생성
   // 장바구니에 필요한 state(예를들어서 td에 들어갈 변하는 값들)들이 App,Detail,Cart 어디에서나 필요하다면? -> 원래는 최상위에 state보관 -> 상태관리 라이브러리 필요
   return (
@@ -15,12 +27,16 @@ export default function Cart() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>안녕</td>
-            <td>안녕</td>
-            <td>안녕</td>
-          </tr>
+          {products?.map((item) => (
+            <>
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.count}</td>
+                <td>?</td>
+              </tr>
+            </>
+          ))}
         </tbody>
       </Table>
     </>
--- react/shop/src/store.js
+++ react/shop/src/store.js
@@ -1,6 +1,30 @@
-// redux 사용시, state 값들을 보관하는 '통'을 만들어준다. store로 작명.
-import { configureStore } from "@reduxjs/toolkit";
+// redux 사용시, state들을 보관하는 '통'인 store를 만들어준다. store로 작명.
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
+//useState랑 거의 비슷하다고 생각하면 된다. createSlice는 cofigureStore에 등록해 사용해야한다.
+
+let user = createSlice({
+  name: "user",
+  initialState: "kim",
+});
+
+let stock = createSlice({
+  name: "stock",
+  initialState: [10, 11, 12],
+});
+
+let products = createSlice({
+  name: "products",
+  initialState: [
+    { id: 0, name: "White and Black", count: 2 },
+    { id: 2, name: "Grey Yordan", count: 1 },
+  ],
+});
+// confiureStore는 slice를 등록하는 곳이다. 규격에 맞춰야함. => 변수.reducer가 store에 등록하는 행동이며, 왼쪽 작명이름은 보통 변수와 이름을 맞춘다. 다른 파일에서 useSelector로 사용할때에는 왼쪽 이름을 끌어다 쓴다.
 export default configureStore({
-  reducer: {},
+  reducer: {
+    user: user.reducer,
+    stock: stock.reducer,
+    products: products.reducer,
+  },
 });
